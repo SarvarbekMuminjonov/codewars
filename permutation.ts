10abbd32f19f6602f6ab475712718384cb86201f
Sarvarbek Moʻminjonov <72138770+SarvarbekMuminjonov@users.noreply.github.com>
2025-10-13 15:11:34 +0500

    Create permutation.ts
    added solution for permutations excercises from leetcode

---
+++ permutation.ts
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/permutations/description/
+function permute(nums: number[]): number[][] {
+    if (nums.length === 1) {
+        return [nums]
+    }
+
+    const combinations = new Array(nums.length).fill(0);
+    const final =new Array(fibonacci(nums.length)).fill(0);
+    let clone = [...nums]
+
+    for (let i = 0; i < nums.length; i++) {
+        clone = shift(clone)
+        combinations.push(clone);
+    }
+
+    for (let i = 0; i < combinations.length; i++) {
+        const result = permute(combinations[i].slice(1));
+
+        for (let j = 0; j < result.length; j++) {
+            final.push([combinations[i][0], ...result[j]])
+        }
+    }
+
+    return final
+}
+
+function shift(nums: number[]): number[] {
+    return [...nums.slice(1),nums[0]]
+}
+
+console.log(permute([1,2,3]))
